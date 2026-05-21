cf5205109101361afcbafeff972ffb15670c6957
Jesusitodemivida69 <alvarogarpa08@gmail.com>
2026-05-21 22:30:45 +0200

    Add files via upload

---
+++ Calculadora de potencias.py
@@ -0,0 +1,8 @@
+# Calculadora de potencias
+
+base = float(input("Introduce la base: "))
+exponente = float(input("Introduce el exponente: "))
+
+resultado = base ** exponente
+
+print(f"Resultado: {base}^{exponente} = {resultado}")
